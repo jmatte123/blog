dc1cff405b89013a50ab68a40946d2b3f191ab20
joematte <mattesoj@augsburg.edu>
2020-10-24 11:06:24 -0500

    sorting for query

--- server/src/route.js
+++ server/src/route.js
@@ -9,7 +9,7 @@ router.use(function(req, res, next) {
 });
 
 router.get("/posts", async (req, res) => {
-	const posts = await Post.find({});
+	const posts = await Post.find({}).sort({date: 'descending'});
 	res.send(posts);
 });
 
